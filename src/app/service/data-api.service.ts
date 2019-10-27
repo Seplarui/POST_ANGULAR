00c17b638c8948c17e59a7dfbcd94235b9212101
Sebas <seplarui@gmail.com>
2019-10-27 20:20:21 +0100

    add api-service/save-empresa-component

--- src/app/service/data-api.service.ts
+++ src/app/service/data-api.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/internal/Observable';
+
+import { EmpresaInterface } from '../../models/empresa-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  empresas: Observable<any>;
+  empresa: Observable<any>;
+  headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
+
+
+  saveEmpresa(empresa: EmpresaInterface) {
+    const urlApi = 'http://localhost:3000/api/empresa';
+    return this.http
+      .post<EmpresaInterface>(urlApi, empresa, { headers: this.headers })
+      .pipe(map(data => data));
+  }
 }
