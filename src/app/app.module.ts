1375a663809ade1d516ef445794a153e01f4fe65
Sebas <seplarui@gmail.com>
2019-10-27 09:17:37 +0100

    apiservice/save-empresa-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SaveEmpresaComponent } from './components/save-empresa/save-empresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SaveEmpresaComponent
   ],
   imports: [
     BrowserModule,
