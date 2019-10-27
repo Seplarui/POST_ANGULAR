497e794f4ace0843e280b5fb6c5ebd64a29d2f56
Sebas <seplarui@gmail.com>
2019-10-27 21:05:09 +0100

    save-empresa-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SaveEmpresaComponent } from './components/save-empresa/save-empresa.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { SaveEmpresaComponent } from './components/save-empresa/save-empresa.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/save-empresa/save-empresa.component.ts
+++ src/app/components/save-empresa/save-empresa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from 'src/app/service/data-api.service';
+import { NgForm } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-save-empresa',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaveEmpresaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataApiService: DataApiService, private location: Location) { }
 
   ngOnInit() {
+  }
+
+  onSaveEmpresa(empresaForm: NgForm): void {
+    console.log('INPUTCIF ' + empresaForm.value.inputCIF);
+    this.dataApiService.saveEmpresa(empresaForm.value).subscribe(empresa => location.reload());
   }
 
 }
